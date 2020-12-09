e1d5527bfd542e76d15b75363a526eb6ad7593e3
Yu, Hugo <shiqiyu2@illinois.edu>
2020-12-09 13:41:17 -0600

    Update client.py

--- client.py
+++ client.py
@@ -3,9 +3,9 @@ import time
 host = '192.168.50.219'
 port = 8083
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1) #在客户端开启心跳维护
+client.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
 client.connect((host, port))
 while True:
     client.send('hello world\r\n'.encode())
     print('send data')
-    time.sleep(1) #如果想验证长时间没发数据，SOCKET连接会不会断开，则可以设置时间长一点
+    time.sleep(1)
