41a75244a0e515a96270bc44c954a91230012dd8
Yu, Hugo <shiqiyu2@illinois.edu>
2020-12-09 13:50:17 -0600

    TCP-time

--- client.py
+++ client.py
@@ -1,11 +1,12 @@
 import socket
 import time
-host = '192.168.50.219'
+host = '192.168.50.219'  # IP of edge tpu
+host = 'localhost'
 port = 8083
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
 client.connect((host, port))
 while True:
-    client.send('hello world\r\n'.encode())
+    client.send(time.asctime(time.localtime(time.time())).encode())
     print('send data')
-    time.sleep(1)
+    time.sleep(10)
