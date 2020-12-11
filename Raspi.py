cf099bcc09aa172c4a17cc95376e81f3e69a7940
Yu, Hugo <shiqiyu2@illinois.edu>
2020-12-11 02:11:27 -0600

    Update Raspi.py

--- Raspi.py
+++ Raspi.py
@@ -76,16 +76,14 @@ def edge_tpu():
             # if sunrise() and not curtain.open:
             #     curtain.open()
 
-def web_server():
-    return 0
-
 if __name__ == '__main__':
         #edge_tpu_thread = threading.Thread(target=edge_tpu)
         #edge_tpu_process = Process(target=edge_tpu)
         #server_thread = threading.Thread(target=web_server)
-        server_process = Process(target=web_server)
+        web_server()
+        #server_process = Process(target=web_server)
         #edge_tpu_process.start()
-        server_process.start()
+        #server_process.start()
         #edge_tpu_process.join()
         #server_process.join()
 
