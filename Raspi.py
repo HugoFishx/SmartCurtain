3738a8a3abe8cfc95f610b6883bb4211f45234d6
Hugo <shiqiyu2@illinois.edu>
2020-12-11 00:32:59 -0600

    raspi

--- Raspi.py
+++ Raspi.py
@@ -6,27 +6,26 @@ import os
 import sys
 import struct
 from picamera import PiCamera
+from time import sleep
 
-def socket_client():
+def socket_client(camera):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         filepath = '/home/pi/Desktop/cap.jpg'
         s.connect(('192.168.50.248',12345))
-        # s.connect(('localhost',12346))
+# s.connect(('localhost',12346))
     except socket.error as msg:
         print(msg)
         sys.exit(1)
 
     while 1:
-        camera = PiCamera()
-        sleep(2)
         camera.capture('/home/pi/Desktop/cap.jpg')
+        sleep(5)
         # filepath = '/Users/yushiqi/Documents/GitHub/SmartCurtain/file'
         if os.path.isfile(filepath):
             # 定义定义文件信息。128s表示文件名为128bytes长，l表示一个int或log文件类型，在此为文件大小
-            fileinfo_size = struct.calcsize('128sl')
             # 定义文件头信息，包含文件名和文件大小
-            fhead = struct.pack('128sl', bytes(os.path.basename(filepath).encode('utf-8')),os.stat(filepath).st_size)
+            fhead = struct.pack(b'128sq', bytes(os.path.basename(filepath).encode('utf-8')),os.stat(filepath).st_size)
             s.send(fhead)
             print ('client filepath: {0}'.format(filepath))
             fp = open(filepath, 'rb')
@@ -36,11 +35,15 @@ def socket_client():
                     print ('{0} file send over...'.format(filepath))
                     break
                 s.send(data)
-                print('send completed, wating for result')
-        print(s.recv(1024))
+        print('send completed, wating for result')
+        if s.recv(1024) == b'1':
+            print('BB here!')
+        else:
+            print('BB not here!')
         s.close()
         break
 
 if __name__ == '__main__':
-    while 1:
-        socket_client()
+        camera = PiCamera()
+        while 1:
+            socket_client(camera)
