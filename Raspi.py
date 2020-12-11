d5d6d5b5fbee8383b71fddce3ce51bdd7d48020c
Yu, Hugo <shiqiyu2@illinois.edu>
2020-12-11 02:10:32 -0600

    Update Raspi.py

--- Raspi.py
+++ Raspi.py
@@ -81,13 +81,13 @@ def web_server():
 
 if __name__ == '__main__':
         #edge_tpu_thread = threading.Thread(target=edge_tpu)
-        edge_tpu_process = Process(target=edge_tpu)
+        #edge_tpu_process = Process(target=edge_tpu)
         #server_thread = threading.Thread(target=web_server)
         server_process = Process(target=web_server)
-        edge_tpu_process.start()
+        #edge_tpu_process.start()
         server_process.start()
-        edge_tpu_process.join()
-        server_process.join()
+        #edge_tpu_process.join()
+        #server_process.join()
 
             
             
