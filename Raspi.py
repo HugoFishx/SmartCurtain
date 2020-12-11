ff7139d347befc390c59af0400a63a052b103a0b
Yu, Hugo <shiqiyu2@illinois.edu>
2020-12-11 02:13:34 -0600

    Update Raspi.py

--- Raspi.py
+++ Raspi.py
@@ -77,10 +77,11 @@ def edge_tpu():
             #     curtain.open()
 
 if __name__ == '__main__':
-        #edge_tpu_thread = threading.Thread(target=edge_tpu)
+        edge_tpu_thread = threading.Thread(target=edge_tpu)
+        server_thread = threading.Thread(target=web_server)
+        edge_tpu_thread.start()
+        server_thread.start()
         #edge_tpu_process = Process(target=edge_tpu)
-        #server_thread = threading.Thread(target=web_server)
-        web_server()
         #server_process = Process(target=web_server)
         #edge_tpu_process.start()
         #server_process.start()
