df6f2564deaaa980b34fcb54cc14cc51db2b248b
Yu, Hugo <shiqiyu2@illinois.edu>
2020-12-11 18:23:41 -0600

    html

--- WebServer.py
+++ WebServer.py
@@ -59,7 +59,7 @@ class CurtainCloseHandler(tornado.web.RequestHandler):
         self.render('index.html', status=status, curtain_status=curtain_status)
 
 # urls = [(r"/", IndexHandler),(r"/open", CurtainOpenHandler),(r"/close", CurtainCloseHandler),(r"/(pic.png)", tornado.web.StaticFileHandler, {'path':'./'})]
-urls = [(r"/", IndexHandler),(r"/open", CurtainOpenHandler),(r"/close", CurtainCloseHandler),(r"/(/home/pi/Desktop/cap.jpg)", tornado.web.StaticFileHandler, {'path':'/home/pi/Desktop/cap.jpg'})]
+urls = [(r"/", IndexHandler),(r"/open", CurtainOpenHandler),(r"/close", CurtainCloseHandler),(r"/(cap.jpg)", tornado.web.StaticFileHandler, {'path':'/home/pi/Desktop'})]
 def web_server(curtain):
     print('server starts')
     tornado.options.parse_command_line()
