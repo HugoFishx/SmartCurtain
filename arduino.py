ebf67b3362b4577d6f7b4e3db517f77ad865f714
Yu, Hugo <shiqiyu2@illinois.edu>
2020-12-12 00:24:33 -0600

    Update arduino.py

--- arduino.py
+++ arduino.py
@@ -1,11 +1,11 @@
 import serial    #import serial module
-ser = serial.Serial('/dev/ttyACM1', 9600,timeout=1);   #open named port at 9600,1s timeot
+ser = serial.Serial('/dev/ttyACM1', 9600,timeout=1)   #open named port at 9600,1s timeot
 
 #try and exceptstructure are exception handler
 try:
   while 1:
-    ser.write('s');#writ a string to port
-    response = ser.readall();#read a string from port
-    print response;
+    ser.write('s')#writ a string to port
+    response = ser.readall()#read a string from port
+    print (response)
 except:
-ser.close();
+    ser.close()
